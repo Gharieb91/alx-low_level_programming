da1d1f137415e05131ae4cdfb212660127f00920
Mohammed Gharieb <drgharieb7@gmail.com>
2023-09-19 00:25:15 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,28 +1,30 @@
- 0; i < 10; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			k = j * i;
-			if (j == 0)
-			{
-				_putchar(k + '0');
-			}
+#include "main.h"
 
-			if (k < 10 && j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			} else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
-		}
-		_putchar('\n');
-	}
+int main() {
+    int i, j, k;
+
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            k = j * i;
+            if (j == 0) {
+                putchar(k + '0');
+            }
+
+            if (k < 10 && j != 0) {
+                putchar(',');
+                putchar(' ');
+                putchar(' ');
+                putchar(k + '0');
+            } else if (k >= 10) {
+                putchar(',');
+                putchar(' ');
+                putchar((k / 10) + '0');
+                putchar((k % 10) + '0');
+            }
+        }
+        putchar('\n');
+    }
+
+    return 0;
 }
 
